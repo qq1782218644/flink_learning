3597c25ab8827854e0e76ea2e982a7d79f723eb8
litao <tao.li@ushow.media>
2023-05-25 00:05:11 +0800

    demo

--- src/main/java/com/litao/flink/chap3/transform/base/AggBaseReduce.java
+++ src/main/java/com/litao/flink/chap3/transform/base/AggBaseReduce.java
@@ -2,7 +2,10 @@ package com.litao.flink.chap3.transform.base;
 
 import com.litao.flink.utils.Record;
 import com.litao.flink.utils.RecordSource;
+import com.litao.flink.utils.WaterSensor;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -15,15 +18,23 @@ public class AggBaseReduce {
          * 3.max
          * 4.minBy
          * 5.maxBy
-         * 需要注意的是  : sum,min,max只对指定字段进行聚合,minBy和maxBy则对符合条件的整条数据进行保留 , flink聚合函数会为每个key值保留一个状态,聚合进行累积
+         * 需要注意的是  :
+         * min,max,sum 只对指定字段记录状态,其他字段值取第一条
+         * minBy maxBy 对min或max的整条数据保留
          */
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStreamSource<Record> ds = env.addSource(new RecordSource());
+        DataStreamSource<WaterSensor> ds = env.fromElements(
+                new WaterSensor("sensor_1", 1L, 1),
+                new WaterSensor("sensor_1", 2L, 2),
+                new WaterSensor("sensor_1", 3L, 3),
+                new WaterSensor("sensor_1", 4L, 4)
+        );
+
 
         ds.keyBy(record -> record.id)
-                .max("timestamp")
-                .print();
+                .sum("ts")
+                .print("min");
 
 
         env.execute();
